4c5acb5920301d63262e05d89259e51d84d97e33
Justin Britt <britt@uwindsor.ca>
2021-04-23 04:16:29 -0400

    Added comment

--- DiscreteHalfNormal/Classes/Calculations/qθMeanCalculation.cs
+++ DiscreteHalfNormal/Classes/Calculations/qθMeanCalculation.cs
@@ -35,6 +35,20 @@
 
         private IxCpCalculation xCpCalculation { get; }
 
+        /// <summary>
+        /// This loops over a range of x, q, and θ values to find calculated means that are within the tolerance of a target mean.
+        /// Note that 0 < q < 1 and 0 < θ.
+        /// </summary>
+        /// <param name="qLowerBound">Lower bound for q</param>
+        /// <param name="qStep">Step for q</param>
+        /// <param name="qUpperBound">Upper bound for q</param>
+        /// <param name="targetMean">Target mean</param>
+        /// <param name="xUpperBound">Upper bound for x</param>
+        /// <param name="θLowerBound">Lower bound for θ</param>
+        /// <param name="θStep">Step for θ</param>
+        /// <param name="θUpperBound">Upper bound for θ</param>
+        /// <param name="tolerance">Tolerance</param>
+        /// <returns>ReadOnlySpan of qθMeanCalculationElement values where |targetMean - calculatedMean| <= tolerance/returns>
         public ReadOnlySpan<qθMeanCalculationElement> Calculate(
             double qLowerBound,
             double qStep,
@@ -46,9 +60,6 @@
             double θUpperBound,
             double tolerance)
         {
-            // 0 < q < 1
-            // 0 < θ
-
             int numberAccepted = 0;
 
             qθMeanCalculationElement[] qθMeanArray = new qθMeanCalculationElement[
