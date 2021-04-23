efb9044611f070dc804325ca513e3e5462af5c46
Justin Britt <britt@uwindsor.ca>
2021-04-23 10:56:04 -0400

    Added comment

--- DiscreteHalfNormal/Interfaces/Calculations/ICCalculation.cs
+++ DiscreteHalfNormal/Interfaces/Calculations/ICCalculation.cs
@@ -6,6 +6,12 @@
 
     public interface ICCalculation
     {
+        /// <summary>
+        /// Calculates the normalization constant C.
+        /// </summary>
+        /// <param name="xCp">x, C * p</param>
+        /// <param name="xUpperBound">Upper bound for x</param>
+        /// <returns>Normalization constant C</returns>
         double Calculate(
             ReadOnlySpan<xCpCalculationElement> xCp,
             int xUpperBound);
