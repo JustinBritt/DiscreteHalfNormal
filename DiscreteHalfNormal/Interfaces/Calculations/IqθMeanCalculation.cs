450a7ac52495497eab6fd175948db488a659ac05
Justin Britt <britt@uwindsor.ca>
2021-04-23 04:23:35 -0400

    Added comment

--- DiscreteHalfNormal/Interfaces/Calculations/IqθMeanCalculation.cs
+++ DiscreteHalfNormal/Interfaces/Calculations/IqθMeanCalculation.cs
@@ -8,6 +8,20 @@
     {
         IxpCalculation xpCalculation { get; }
 
+        /// <summary>
+        /// This loops over a range of x, q, and θ values to find calculated means that are within the tolerance of a target mean.
+        /// Note that 0 < q < 1 and 0 < θ.
+        /// </summary>
+        /// <param name="qLowerBound">Lower bound for q</param>
+        /// <param name="qStep">Step for q</param>
+        /// <param name="qUpperBound">Upper bound for q</param>
+        /// <param name="targetMean">Target mean</param>
+        /// <param name="xUpperBound">Upper bound for x</param>
+        /// <param name="θLowerBound">Lower bound for θ</param>
+        /// <param name="θStep">Step for θ</param>
+        /// <param name="θUpperBound">Upper bound for θ</param>
+        /// <param name="tolerance">Tolerance</param>
+        /// <returns>ReadOnlySpan of <see cref="qθMeanCalculationElement"/> values where |<paramref name="targetMean"/> - calculatedMean| <= <paramref name="tolerance"/></returns>
         ReadOnlySpan<qθMeanCalculationElement> Calculate(
             double qLowerBound,
             double qStep,
